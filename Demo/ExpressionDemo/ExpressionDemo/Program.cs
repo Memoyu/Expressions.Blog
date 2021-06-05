7e05df4854a018099bf69d1274e40bffac6fd7b2
Memoyu <mmy6076@outlook.com>
2021-06-05 16:11:30 +0800

    feat: 添加Demo

---
+++ Demo/ExpressionDemo/ExpressionDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+            ConstantExpression five = Expression.Constant(5, typeof(int));
+            BinaryExpression numLessThanFive = Expression.LessThan(numParam, five);
+            Expression<Func<int, bool>> lambda1 = Expression.Lambda<Func<int, bool>>( numLessThanFive, new ParameterExpression[] { numParam });
+        }
+    }
+
+
+    class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }   
+}
