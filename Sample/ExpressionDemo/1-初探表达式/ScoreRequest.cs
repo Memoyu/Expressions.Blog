021288894a6d8fbdbe9943ec70cd656740db1056
Memoyu <mmy6076@outlook.com>
2021-06-06 23:35:02 +0800

    some

---
+++ Sample/ExpressionDemo/1-初探表达式/ScoreRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace _1_初探表达式
+{
+    internal class ScoreRequest
+    {
+        public ScoreRequest()
+        {
+        }
+
+        public string CourseName { get; set; }
+        public string StudentName { get; set; }
+    }
+}
